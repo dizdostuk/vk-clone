123cf0b27fa7ca030926aa86f25876cc37aad5dd
Ulan <diz.dostuk@yandex.ru>
2019-01-10 22:00:38 +0330

    Added notify menu block

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -22,7 +22,30 @@ const Navbar = () => {
           <div className="grid-item grid-notify-music">
             <div className="notifications">
               <i className="fa fa-bell" />
+              <div className="nav-notification-wrap">
+                <div className="nav-notify-header">
+                  <div className="nav-notify-header_label">
+                    <span className="nav-notify-header_label_urpage">
+                      Ваша страница
+                    </span>
+                  </div>
+                  <Link
+                    to="/settings?act=notify"
+                    className="nav-notify-header_link"
+                  >
+                    Настройки
+                  </Link>
+                </div>
+                <div className="nav-notify-events">
+                  <div className="nav-notify_event">
+                    <img src="" alt="" className="nav-notify_event_image"/>
+                    <div className="nav-notify_event_description"></div>
+                    <div className="nav-notify_event_submenu"></div>
+                  </div>
+                </div>
+              </div>
             </div>
+
             <div className="music">
               <i className="fa fa-music" />
             </div>
@@ -40,13 +63,23 @@ const Navbar = () => {
               </div>
             </div>
             <div className="nav-profile-menu">
-              <Link className="nav-profile-menu_links" to="/mypage">Моя страница</Link>
-              <div className="nav-profile-sep"></div>
-              <Link className="nav-profile-menu_links" to="/edit">Редактировать</Link>
-              <Link className="nav-profile-menu_links" to="/settings">Настройки</Link>
-              <Link className="nav-profile-menu_links" to="/support">Помощь</Link>
-              <div className="nav-profile-sep"></div>
-              <Link className="nav-profile-menu_links" to="/exit">Выход</Link>
+              <Link className="nav-profile-menu_links" to="/mypage">
+                Моя страница
+              </Link>
+              <div className="nav-profile-sep" />
+              <Link className="nav-profile-menu_links" to="/edit">
+                Редактировать
+              </Link>
+              <Link className="nav-profile-menu_links" to="/settings">
+                Настройки
+              </Link>
+              <Link className="nav-profile-menu_links" to="/support">
+                Помощь
+              </Link>
+              <div className="nav-profile-sep" />
+              <Link className="nav-profile-menu_links" to="/exit">
+                Выход
+              </Link>
             </div>
           </div>
         </div>
