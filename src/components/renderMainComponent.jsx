2cea43cd2435a7a1ab8c58e784b4e22dbf429c7f
Ulan <diz.dostuk@yandex.ru>
2019-01-18 22:07:03 +0330

    Post editting again 03

--- src/components/renderMainComponent.jsx
+++ src/components/renderMainComponent.jsx
@@ -8,15 +8,20 @@ class RenderMainComponent extends Component {
     postChanged: false
   };
 
-  handleChangePost = (e) => {
-    if(this.state.postString === "")
-      this.setState({postChanged: false});
-    else
-      this.setState({postChanged: true});
-      
-    this.setState({postString: e.target.value});    
-  }
+  handleChangePost = e => {
+    if (this.state.postString === "") this.setState({ postChanged: false });
+    else this.setState({ postChanged: true });
+
+    this.setState({ postString: e.target.value });
+  };
   render() {
+    const actionBtnStyle = {
+      position: "relative",
+      marginLeft: "-28px",
+      marginRight: "20px",
+      borderTop: "1px solid #e7e8ec",
+      fontSize: "13px"
+    };
     return (
       <div className="wide_column_wrap">
         <div className="wide_column">
@@ -41,15 +46,22 @@ class RenderMainComponent extends Component {
                   className="post_field_user_image"
                 />
               </Link>
+
               <div className="post_field_wrap">
                 <div
                   className="submit_post_field"
                   contentEditable="true"
                   role="textbox"
                   onChange={this.handleChangePost}
-                  dangerouslySetInnerHTML={{__html: this.state.postString}}
+                  dangerouslySetInnerHTML={{ __html: this.state.postString }}
                 />
-                <div className={ !this.state.postChanged ? "placeholder dp_block" : "placeholder dp_none"}>
+                <div
+                  className={
+                    this.state.postString === ""
+                      ? "placeholder dp_block"
+                      : "placeholder dp_none"
+                  }
+                >
                   <div className="plh_input">
                     <div className="plh_content">Что у вас нового?</div>
                   </div>
@@ -59,7 +71,7 @@ class RenderMainComponent extends Component {
                     <i className="fas fa-smile-beam" />
                   </div>
                 </div>
-                <div className="post_actions_btns">
+                <div className="post_actions_btns" style={actionBtnStyle}>
                   <div className="post_action_btn">
                     <div className="post_action_btn_layout">
                       <span className="post_action_btn_text">Видно всем</span>
@@ -74,6 +86,37 @@ class RenderMainComponent extends Component {
                         </div>
                       </div>
                     </div>
+                  </div>
+
+                  <div className="post_action_btn">
+                    <div className="post_action_btn_layout">
+                      <span className="post_action_btn_text">Сейчас</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="submit_post">
+                <div className="poster_media">
+                  <div className="poster__open-btn-wrapper">
+                    <div className="poster__open-btn-layout">
+                      <div className="poster__open-btn" />
+                    </div>
+                  </div>
+
+                  <div className="page_add_media">
+                    <Link to="" className="media_selector_item">
+                      <i className="fa fa-photo" />
+                    </Link>
+                    <Link to="" className="media_selector_item">
+                      <i className="fa fa-youtube" />
+                    </Link>
+                    <Link to="" className="media_selector_item">
+                      <i className="fa fa-music" />
+                    </Link>
+                    <Link to="" className="media_selector_item">
+                      <i className="fa fa-align-right" />
+                    </Link>
                   </div>
                 </div>
               </div>
